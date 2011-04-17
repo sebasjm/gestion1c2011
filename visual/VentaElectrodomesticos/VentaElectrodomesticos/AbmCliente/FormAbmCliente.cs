194d68f117cad0bbe1a7a27e98607b6ed9095246
unknown <hernan@.(none)>
2011-04-17 13:43:37 -0300

    1

---
+++ visual/VentaElectrodomesticos/VentaElectrodomesticos/AbmCliente/FormAbmCliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VentaElectrodomesticos.AbmCliente
+{
+    public partial class FormAbmCliente : Form
+    {
+        public FormAbmCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
