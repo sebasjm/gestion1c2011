7a7fcf4c54ed8d169d13848a2f30b087bbf1ac28
unknown <hernan@.(none)>
2011-04-17 16:16:11 -0300

    Primer ejemplo de conexión de base de datos Se agregaron los ignore para los archivos binarios generados por Visual

--- visual/VentaElectrodomesticos/Login/FormLogin.cs
+++ visual/VentaElectrodomesticos/Login/FormLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace VentaElectrodomesticos.Login
 {
     public partial class FormLogin : Form
@@ -14,6 +16,34 @@ namespace VentaElectrodomesticos.Login
         public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection myConnection = new SqlConnection("user id=gd;" +
+                                       "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=GD1C2011; " +
+                                       "connection timeout=1");
+            try
+            {
+                myConnection.Open();
+                SqlCommand myCommand = new SqlCommand("SELECT TOP 5 cli_nombre, cli_apellido FROM gd_esquema.Maestra", myConnection);
+
+                String resultado = "";
+
+                SqlDataReader myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    resultado = resultado + myReader["cli_nombre"].ToString() + " = " + myReader["cli_apellido"].ToString() + "\n";
+                }
+                label1.Text = "resultado = " + resultado;
+            }
+            catch (Exception es)
+            {
+                label1.Text = es.ToString();
+                Console.WriteLine(es.ToString());
+            }
         }
     }
 }
