d0dc753125184883eac4a4949b10fa9533499870
unknown <rebeca@.(none)>
2011-04-24 19:35:42 -0300

    changing the order of the creation of the tables since you can create a foring key to a table that does not exists yet

--- db/migration/script_creacion_inicial.sql
+++ db/migration/script_creacion_inicial.sql
@@ -25,13 +25,40 @@ CREATE TABLE [la_huerta].[Marca](
 	unique (nombre)
 ) ON [PRIMARY]
 
+CREATE TABLE [la_huerta].[Provincia](
+	[id] [tinyint] NOT NULL,
+	[nombre] [varchar](60) NOT NULL,
+	primary key (id),
+	unique (nombre)
+) ON [PRIMARY]
+
+CREATE TABLE [la_huerta].[TipoSucursal](
+	[id] [tinyint] NOT NULL,
+	[nombre] [varchar](20) NULL,
+	primary key (id),
+	unique (nombre)
+) ON [PRIMARY]
+
+CREATE TABLE [la_huerta].[Sucursal](
+	[id] [int] NOT NULL,
+	[direccion] [varchar](60) NOT NULL,
+	[telefono] [varchar](20) NOT NULL,
+	[tipoSucursal_id] [tinyint] NOT NULL,
+	[provincia_id] [tinyint] NOT NULL,
+	primary key (id),
+    foreign key (tipoSucursal_id) references [la_huerta].[TipoSucursal](id),
+    foreign key (provincia_id) references [la_huerta].[Provincia](id),
+	unique (direccion),
+	unique (telefono)
+) ON [PRIMARY]
+
 CREATE TABLE [la_huerta].[Empleado](
 	[dni] [int] NOT NULL,
 	[nombre] [varchar](50) NOT NULL,
 	[apellido] [varchar](50) NOT NULL,
 	[mail] [varchar](50) NOT NULL,
 	[direccion] [varchar](60) NOT NULL,
-	[tipoEmpleado_id] [int] NOT NULL,
+	[tipoEmpleado_id] [tinyint] NOT NULL,
 	[sucursal_id] [int] NOT NULL,
     [activo] [bit] NOT NULL DEFAULT 1,
     [usuario_id] [int] NOT NULL,
@@ -50,17 +77,14 @@ CREATE TABLE [la_huerta].[Usuario](
 	unique (username)
 ) ON [PRIMARY]
 
-CREATE TABLE [la_huerta].[Sucursal](
-	[id] [int] NOT NULL,
-	[direccion] [varchar](60) NOT NULL,
-	[telefono] [varchar](20) NOT NULL,
-	[tipoSucursal_id] [smallint] NOT NULL,
-	[provincia_id] [tinyint] NOT NULL,
+CREATE TABLE [la_huerta].[Categoria](
+	[id] [smallint] NOT NULL,
+	[categoria_padre] [smallint] NULL,
+	[nombre] [varchar](10) NOT NULL,
+	[descripcion] [varchar](50) NOT NULL,
 	primary key (id),
-    foreign key (tipoSucursal_id) references [la_huerta].[TipoSucursal](id),
-    foreign key (provincia_id) references [la_huerta].[Provincia](id),
-	unique (direccion),
-	unique (telefono)
+    foreign key (categoria_padre) references [la_huerta].[Categoria](id),
+	unique (nombre)
 ) ON [PRIMARY]
 
 CREATE TABLE [la_huerta].[Producto](
@@ -69,7 +93,7 @@ CREATE TABLE [la_huerta].[Producto](
 	[descripcion] [varchar](50) NULL,
 	[precio] [float] NOT NULL,
 	[marca_id] [smallint] NOT NULL,
-	[categoria_id] [int] NOT NULL,
+	[categoria_id] [smallint] NOT NULL,
 	primary key (id),
     foreign key (marca_id) references [la_huerta].[Marca](id),
     foreign key (categoria_id) references [la_huerta].[Categoria](id),
@@ -114,28 +138,12 @@ CREATE TABLE [la_huerta].[Pago](
     foreign key (empleado_dni) references [la_huerta].[Empleado](dni)
 ) ON [PRIMARY]
 
-CREATE TABLE [la_huerta].[UsuarioRol](
-	[usuario_id] [int] NOT NULL,
-	[rol_id] [tinyint] NOT NULL,
-	primary key (usuario_id,rol_id),
-    foreign key (usuario_id) references [la_huerta].[Usuario](id),
-    foreign key (rol_id) references [la_huerta].[Rol](id)
-) ON [PRIMARY]
-
 CREATE TABLE [la_huerta].[Rol](
 	[id] [tinyint] NOT NULL,
 	[nombre] [varchar](10) NOT NULL,
 	[descripcion] [varchar](50) NOT NULL,
 	primary key (id),
 	unique (nombre)
-) ON [PRIMARY]
-
-CREATE TABLE [la_huerta].[RolFuncionalidad](
-	[rol_id] [tinyint] NOT NULL,
-	[funcionalidad_id] [tinyint] NOT NULL,
-	primary key (rol_id,funcionalidad_id),
-    foreign key (rol_id) references [la_huerta].[Rol](id),
-    foreign key (funcionlidad_id) references [la_huerta].[Funcionalidad](id)
 ) ON [PRIMARY]
 
 CREATE TABLE [la_huerta].[Funcionalidad](
@@ -146,14 +154,20 @@ CREATE TABLE [la_huerta].[Funcionalidad](
 	unique (nombre)
 ) ON [PRIMARY]
 
-CREATE TABLE [la_huerta].[Categoria](
-	[id] [smallint] NOT NULL,
-	[categoria_padre] [smallint] NULL,
-	[nombre] [varchar](10) NOT NULL,
-	[descripcion] [varchar](50) NOT NULL,
-	primary key (id),
-    foreign key (categoria_padre) references [la_huerta].[Categoria](id),
-	unique (nombre)
+CREATE TABLE [la_huerta].[UsuarioRol](
+	[usuario_id] [int] NOT NULL,
+	[rol_id] [tinyint] NOT NULL,
+	primary key (usuario_id,rol_id),
+    foreign key (usuario_id) references [la_huerta].[Usuario](id),
+    foreign key (rol_id) references [la_huerta].[Rol](id)
+) ON [PRIMARY]
+
+CREATE TABLE [la_huerta].[RolFuncionalidad](
+	[rol_id] [tinyint] NOT NULL,
+	[funcionalidad_id] [tinyint] NOT NULL,
+	primary key (rol_id,funcionalidad_id),
+    foreign key (rol_id) references [la_huerta].[Rol](id),
+    foreign key (funcionalidad_id) references [la_huerta].[Funcionalidad](id)
 ) ON [PRIMARY]
 
 CREATE TABLE [la_huerta].[ItemFactura](
@@ -166,16 +180,3 @@ CREATE TABLE [la_huerta].[ItemFactura](
     foreign key (producto_id) references [la_huerta].[Producto](id)
 ) ON [PRIMARY]
 
-CREATE TABLE [la_huerta].[Provincia](
-	[id] [tinyint] NOT NULL,
-	[nombre] [varchar](60) NOT NULL,
-	primary key (id),
-	unique (nombre)
-) ON [PRIMARY]
-
-CREATE TABLE [la_huerta].[TipoSucursal](
-	[id] [tinyint] NOT NULL,
-	[nombre] [varchar](20) NULL,
-	primary key (id),
-	unique (nombre)
-) ON [PRIMARY]
