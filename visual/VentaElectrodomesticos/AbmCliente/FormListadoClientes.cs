a5203eae97ff7d5bdb1b965eb03917a44425c6e0
unknown <hernan@.(none)>
2011-05-01 14:45:14 -0300

    Agrego los abms vista

--- visual/VentaElectrodomesticos/AbmCliente/FormListadoClientes.cs
+++ visual/VentaElectrodomesticos/AbmCliente/FormListadoClientes.cs
@@ -6,19 +6,116 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
 
 namespace VentaElectrodomesticos.AbmCliente
 {
     public partial class FormListadoClientes : Form
     {
+        private int messageFromParent;
+        public int MessageFromParent
+        {
+            get { return messageFromParent; }
+            set { messageFromParent = value; }
+        }
+
+        private string nombre;
+        private string apellido;
+        private int dni;
+
         public FormListadoClientes()
         {
             InitializeComponent();
+            FillData();
+            FillProvincias();
         }
 
-        private void grpFiltrosBusqueda_Enter(object sender, EventArgs e)
+        void FillData()
         {
+            SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                              "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                              "database=GD1C2011; ");
+            try
+            {
 
+                String strDNI = "";
+                if (dni != 0){ strDNI = " AND dni=" + dni  ; }
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.cliente WHERE " +
+                    "  LOWER(nombre) like  LOWER('%" +nombre+ "%') AND" +
+                    "  LOWER(apellido) like  LOWER('%" +apellido+"%')" +
+                    strDNI
+                    , sqlConnection);
+                sqlConnection.Open();
+                DataSet ds1 = new DataSet();
+                SqlDataAdapter adp1 = new SqlDataAdapter(sqlCommand);
+                adp1.Fill(ds1, "customer");
+                dataClientes.DataSource = ds1;
+                dataClientes.DataMember = "customer";
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
         }
-    }
+        void FillProvincias()
+        {
+           SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                              "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                              "database=GD1C2011; ");
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.provincia", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader TheReader1 = sqlCommand.ExecuteReader();
+                // Creamos un nuevo objeto DataTable
+                DataTable dt = new DataTable();
+                // Cargamos los datos del lector en el objeto DataTable
+                dt.Load(TheReader1);
+                // Configuramos el primer control ComboBox Provincias
+                cmbProvincia.DataSource = dt;
+                cmbProvincia.DisplayMember = "nombre";
+                cmbProvincia.ValueMember = "id";
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void bLimpiar_Click(object sender, EventArgs e)
+        {
+            txtApellido.Text = "";
+            txtDni.Text = "";
+            txtNombre.Text = "";
+            cmbProvincia.SelectedIndex = -1;
+        }
+
+        private void bCancelar_Click(object sender, EventArgs e)
+        {
+            this.messageFromParent = 0;
+            this.Close();
+        }
+        private void bSeleccionar_Click(object sender, EventArgs e)
+        {
+            int dni =  System.Int32.Parse(dataClientes["dni", dataClientes.CurrentRow.Index].Value.ToString());
+            this.messageFromParent = dni;
+            this.Close();
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+            this.nombre = txtNombre.Text;
+            this.apellido = txtApellido.Text;
+            if (txtDni.Text == "") { this.dni = 0; }
+            else {
+            this.dni = System.Int32.Parse(txtDni.Text);}
+            FillData();
+        }
+   }
 }
--- visual/VentaElectrodomesticos/AbmEmpleado/FormAbmEmpleado.cs
+++ visual/VentaElectrodomesticos/AbmEmpleado/FormAbmEmpleado.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace VentaElectrodomesticos.AbmEmpleado
 {
@@ -14,15 +16,171 @@ namespace VentaElectrodomesticos.AbmEmpleado
         public FormAbmEmpleado()
         {
             InitializeComponent();
+            FillProvincias();
+            FillSucursal();
+            FillTipoEmpleado();
         }
 
         private void bBuscar_Click(object sender, EventArgs e)
         {
             FormListadoEmpleados form = new FormListadoEmpleados();
+            form.MessageFromParent = 0;
             form.ShowDialog(this);
+            if (form.MessageFromParent != 0)
+            {
+                this.cargarEmpleado(form.MessageFromParent);
+            }
+            //MessageBox.Show(form.MessageFromParent.ToString());
+            bCrearOtro.Hide();
+            bLimpiar.Text = "Borrar";
+            bCrear.Text = "Modificar";
         }
 
-    
+        public void cargarEmpleado(long dni)
+        {
+            SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                              "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                              "database=GD1C2011; ");
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.empleado WHERE dni=" + dni
+                    , sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader dataReader = null;
+                dataReader = sqlCommand.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    if (dataReader.Read())
+                    {
+                        txtApellido.Text = dataReader["apellido"].ToString();
+                        txtNombre.Text = dataReader["nombre"].ToString();
+                        txtDni.Text = dataReader["dni"].ToString();
+                        txtMail.Text = dataReader["mail"].ToString();
+                        //txtTelefono.Text = dataReader["telefono"].ToString();
+                        //txtCalle.Text = dataReader["direccion"].ToString();
+
+                        string input = dataReader["direccion"].ToString();
+                        // Split on one or more non-digit characters.
+                        string[] numbers = Regex.Split(input, @"\D+");
+                        txtCalle.Text = Regex.Split(input, numbers[1])[0];
+                        txtNumero.Text = (numbers[1] != "") ? "" + numbers[1] : "";
+                        txtDeptoPiso.Text = (numbers[2] != "") ? "" + numbers[2] : "";
+                    }
+                }
+                sqlConnection.Close();
+            }
+            catch (IndexOutOfRangeException) { /* TODO : Error que no se porque sucede */  }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+        void FillProvincias()
+        {
+            SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                               "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                               "database=GD1C2011; ");
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.provincia", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader TheReader1 = sqlCommand.ExecuteReader();
+                // Creamos un nuevo objeto DataTable
+                DataTable dt = new DataTable();
+                // Cargamos los datos del lector en el objeto DataTable
+                dt.Load(TheReader1);
+                // Configuramos el primer control ComboBox Provincias
+                cmbProvincia.DataSource = dt;
+                cmbProvincia.DisplayMember = "nombre";
+                cmbProvincia.ValueMember = "id";
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+        void FillSucursal()
+        {
+            SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                               "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                               "database=GD1C2011; ");
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.sucursal", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader TheReader1 = sqlCommand.ExecuteReader();
+                // Creamos un nuevo objeto DataTable
+                DataTable dt = new DataTable();
+                // Cargamos los datos del lector en el objeto DataTable
+                dt.Load(TheReader1);
+                // Configuramos el primer control ComboBox Provincias
+                cmbSucursal.DataSource = dt;
+                cmbSucursal.DisplayMember = "nombre";
+                cmbSucursal.ValueMember = "id";
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+        void FillTipoEmpleado()
+        {
+            SqlConnection sqlConnection = new SqlConnection("user id=gd;" +
+                               "password=gd2011;server=localhost\\SQLSERVER2005;" +
+                               "database=GD1C2011; ");
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM la_huerta.tipoempleado", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader TheReader1 = sqlCommand.ExecuteReader();
+                // Creamos un nuevo objeto DataTable
+                DataTable dt = new DataTable();
+                // Cargamos los datos del lector en el objeto DataTable
+                dt.Load(TheReader1);
+                // Configuramos el primer control ComboBox Provincias
+                cmbTipoEmpleado.DataSource = dt;
+                cmbTipoEmpleado.DisplayMember = "nombre";
+                cmbTipoEmpleado.ValueMember = "id";
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error en la Consulta", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+        private void bLimpiar_Click(object sender, EventArgs e)
+        {
+            if (bLimpiar.Text == "Borrar")
+            {
+                if (MessageBox.Show("¿Esta seguro que desea eliminar al cliente?", "Confirmar Eliminación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // proceder con el borrado
+                }
+            }
+
+        }
+
+        private void bCrear_Click(object sender, EventArgs e)
+        {
+            if (bLimpiar.Text == "Crear")
+            {
+                if (MessageBox.Show("¿Esta seguro que desea crear al cliente?", "Confirmar Creación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // proceder con la creacion
+                }
+            }else{
+                if (MessageBox.Show("¿Esta seguro que desea modificar al cliente?", "Confirmar Modificación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // proceder con la modificacion
+                }
+            }
+        }
+
 
 
     }
