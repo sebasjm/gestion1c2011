1406e3dee27cd882b9076ff39931618dd17c8143
unknown <hernan@.(none)>
2011-05-01 15:06:20 -0300

    Modificacion del Login le borre el TextChanged y le pase algo a español

--- visual/VentaElectrodomesticos/Login/FormLogin.cs
+++ visual/VentaElectrodomesticos/Login/FormLogin.cs
@@ -6,12 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
 using System.Data.SqlClient;
-
 using VentaElectrodomesticos.Controladores;
 using VentaElectrodomesticos.Exception;
-
 namespace VentaElectrodomesticos.Login
 {
     public partial class FormLogin : Form
@@ -20,23 +17,14 @@ namespace VentaElectrodomesticos.Login
         {
             InitializeComponent();
         }
-
- 
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void bAceptar_Click(object sender, EventArgs e) {
             try {
                 Context.instance.authenticate(tUsername.Text, tPassword.Text);
-            } catch (WrongUserOrPasswordException ex) {
-                tUsername.Text = "Wrong username or password";
+            } catch (WrongUserOrPasswordException ) {
+                tUsername.Text = "Usuario o Contraseña incorrecta";
                 return;
             }
             tUsername.Text = "right!";
         }
-
     }
 }
