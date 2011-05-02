d9f17213a4c2bc5ac7544b96987186cfd4abe602
unknown <hernan@.(none)>
2011-05-02 16:20:24 -0300

    Se agrego el DAO marca y unas mejoras del ABM de producto.

---
+++ visual/VentaElectrodomesticos/Controladores/MarcaDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VentaElectrodomesticos.Modelo;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using VentaElectrodomesticos.Controladores;
+
+namespace VentaElectrodomesticos.Controladores {
+    class MarcaDAO {
+        private Connection connection;
+        public MarcaDAO(Connection connection)
+        {
+            this.connection = connection;
+            Context.instance.dao.addMapper(typeof(Marca), new MarcaMapper());
+        }
+        public List<Marca> search(string nombre)
+        {
+            QueryBuilder q = new QueryBuilder();
+            q.select()
+                .from("la_huerta.marca")
+                .filterIf(nombre.Length != 0, "nombre like '%{0}%' ", nombre);
+            return connection.query<Marca>(q.build(), q.getParams());
+        }
+        class MarcaMapper : Mapper<Object> {
+            public Object getInstance(SqlDataReader sdr) {
+                //MessageBox.Show("ID : " + sdr.GetValue(0) + "NOMBRE :" + sdr.GetValue(1));
+                // TODO : Ver porque tipo empleado tiene un espacio en la primary
+                String cadena = (string)sdr.GetValue(0).ToString().Trim(' ');
+                return new Marca {
+                    id = (int)Int16.Parse(cadena),
+                    nombre = (string)sdr.GetValue(1)
+                };
+            }
+        }
+    }
+}
---
+++ visual/VentaElectrodomesticos/Modelo/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentaElectrodomesticos.Modelo {
+    class Marca {
+        public int id {
+            get; set;
+        }
+        public string nombre {
+            get; set;
+        }
+    }
+}
