ce6ec32b4eefc41228755da13aa812339e16eb41
Sebastian Marchano <sebasjm@computer.org>
2011-05-14 18:42:42 -0300

    bleh

---
+++ visual/VentaElectrodomesticos/Exception/UnkownEnumException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentaElectrodomesticos.Exception {
+    class UnkownEnumException : System.Exception {
+        public UnkownEnumException(String ex)
+            : base(ex)
+        {
+            
+        }
+    }
+}
