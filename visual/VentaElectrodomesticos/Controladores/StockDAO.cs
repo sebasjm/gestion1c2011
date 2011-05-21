2ba78c7c8a8937493781bd0fa3c75798cbcef9e7
Sebastian Marchano <sebasjm@computer.org>
2011-05-21 18:03:34 -0300

    merge

--- visual/VentaElectrodomesticos/Controladores/StockDAO.cs
+++ visual/VentaElectrodomesticos/Controladores/StockDAO.cs
@@ -4,33 +4,26 @@ using System.Linq;
 using System.Text;
 using VentaElectrodomesticos.Modelo;
 using System.Data.SqlClient;
-namespace VentaElectrodomesticos.Controladores
-{
-    class StockDAO
-    {
+namespace VentaElectrodomesticos.Controladores {
+    class StockDAO {
         private Connection connection;
-        public StockDAO(Connection connection)
-        {
+        public StockDAO(Connection connection) {
             this.connection = connection;
             Context.instance.dao.addMapper(typeof(Stock), new StockMapper());
         }
-        class StockMapper : Mapper<Object>
-        {
-            public Object getInstance(SqlDataReader sdr)
-            {
-                return new Stock(sdr.GetByte(0), sdr.GetInt32(1))
-                {
+        class StockMapper : Mapper<Object> {
+            public Object getInstance(SqlDataReader sdr) {
+                return new Stock(sdr.GetByte(0), sdr.GetInt32(1)) {
                     stock = sdr.GetInt32(2)
                 };
             }
         }
-        public List<Stock> search(Sucursal sucursal, Producto producto)
-        {
+        public List<Stock> search(Sucursal sucursal, Producto producto) {
             QueryBuilder q = new QueryBuilder();
             q.select()
                 .from("la_huerta.Stock")
-                .filterIf(sucursal != null, "sucursal_id = {0} ", sucursal != null ? sucursal.id : 0)
-                .filterIf(producto != null, "producto_codigo = {1} ", producto != null ? producto.codigo : 0);
+                .filterIf(sucursal != null && sucursal.id != 0, "sucursal_id = {0} ", sucursal != null ? sucursal.id : 0)
+                .filterIf(producto != null && producto.codigo != 0, "producto_codigo = {1} ", producto != null ? producto.codigo : 0);
 
             return connection.query<Stock>(q.build(), q.getParams());
         }
